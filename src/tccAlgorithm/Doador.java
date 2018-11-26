e99a0272ae7e4fff2e185a5954ac8c61856c3195
Samuel Lucas <samuel1797@gmail.com>
2018-11-25 23:48:32 -0300

    Update Doador.java

--- src/tccAlgorithm/Doador.java
+++ src/tccAlgorithm/Doador.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -5,7 +6,7 @@
  */
 
 /**
- * Representa um doador ou um rimCadaver
+ * Representa um doador vivo ou um rim de um doador cadáver
  * @author  samuellucas97
  *          candinhojr
  * @since   13.11.2018
@@ -13,46 +14,73 @@
 public class Doador {
     
     protected String nomeDoador;
+    protected boolean ehDoadorCadaver;
     protected TiposDeSangue tipoDeSangueDoador;
     protected String listaHLADoador;
 
     /**
-     * 
+     * Construtor padrão
+     */
+    public Doador() {
+    	nomeDoador = new String();
+    	ehDoadorCadaver = true;
+    	tipoDeSangueDoador =  TiposDeSangue.O_negativo;
+    	listaHLADoador = new String();
+    	
+    }
+    
+    /**
+     * Construtor parametrizado
      * @param nomeDoador Nome do doador
+     * @param ehDoadorCadaver Tipo de doador: cadáver ou vivo 
      * @param tipoDeSangueDoador Tipo de sangue do doador
      * @param listaHLADoador Lista de HLA do doador
      */
-    public Doador(String nomeDoador, TiposDeSangue tipoDeSangueDoador, String listaHLADoador) {
+    public Doador(  String nomeDoador,
+                    boolean ehDoadorCadaver, 
+                    TiposDeSangue tipoDeSangueDoador, 
+                    String listaHLADoador) {
         this.nomeDoador = nomeDoador;
+        this.ehDoadorCadaver = ehDoadorCadaver;
         this.tipoDeSangueDoador = tipoDeSangueDoador;
         this.listaHLADoador = listaHLADoador;
     }
     
-    public Doador() {
-        
+    /**
+     * Construtor parametrizado
+     * @param nomeDoador Nome do doador
+     * @param tipoDeSangueDoador Tipo de sangue do doador
+     * @param listaHLADoador Lista de HLA do doador
+     */
+    public Doador(  String nomeDoador,
+                    TiposDeSangue tipoDeSangueDoador, 
+                    String listaHLADoador) {
+        this.nomeDoador = nomeDoador;
+        this.ehDoadorCadaver = true;
+        this.tipoDeSangueDoador = tipoDeSangueDoador;
+        this.listaHLADoador = listaHLADoador;
     }
     
+
     public String getNomeDoador() {
         return nomeDoador;
     }
     
-    public void setNomeDoador(String nomeDoador) {
-        this.nomeDoador = nomeDoador;
+    public boolean getEhDoadorCadaver(){
+    	return ehDoadorCadaver;
     }
-
+    
     public TiposDeSangue getTipoDeSangueDoador() {
         return tipoDeSangueDoador;
-    }
-
-    public void setTipoDeSangueDoador(TiposDeSangue tipoDeSangueDoador) {
-        this.tipoDeSangueDoador = tipoDeSangueDoador;
     }
 
     public String getListaHLADoador() {
         return listaHLADoador;
     }
 
-    public void setListaHLADoador(String listaHLADoador) {
-        this.listaHLADoador = listaHLADoador;
+    @Override
+    public String toString(){
+        return "" + nomeDoador + " " + tipoDeSangueDoador + " " + listaHLADoador;
     }
+
 }
