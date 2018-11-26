b73b87ae4fbf5ecbcc92c280fed561c9f583f427
Samuel Lucas <samuel1797@gmail.com>
2018-11-26 00:57:11 -0300

    Update Vertice.java

--- src/tccAlgorithm/Vertice.java
+++ src/tccAlgorithm/Vertice.java
@@ -6,12 +6,11 @@
 public class Vertice {
 	
 	private Doador chave; /** Chave do vértice */
-	
-	private Arco arcoEmergente; /** Arco que sae desse vértice.
-	 
-	 				Obs.: Não se usou uma lista de arcos pois, como se fará um emparelhamento,
-	 cada vértice poderá emergir dele apenas um arco - o que liga ao vértice contendo doador mais preferido atualmente
-	 */ 
+	private Arco arcoEmergente; 	/** Arco que sae desse vértice.
+					 *
+					 * Obs.: Não se usou uma lista de arcos pois, como se fará um emparelhamento,
+					 * cada vértice poderá emergir dele apenas um arco - o que liga ao vértice contendo doador mais preferido atualmente
+	 				 */ 
 	
 	/**
 	 * Construtor padrão
